920bd48383efb6c25fd93602c1221f57f755b185
KellyAineah <kellyainea99@gmail.com>
2024-03-30 18:10:54 +0300

    added filter method

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers, string){
+    return drivers.filter(driver=> driver.toLowerCase()=== string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(function(driver){
+        return string.substring(0, 1)===driver.substring(0,1)
+    })
+}
+function matchName(drivers, argument){
+    return drivers.filter(function(driver){
+        return driver.name === argument
+    });
+}
+    
